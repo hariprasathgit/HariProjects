2ad1f7fda569672c1a1fb71ccd70ec4f416288fb
hariprasathgit <90201870+hariprasathgit@users.noreply.github.com>
2024-07-02 16:32:17 +0530

    test commit

--- src/com/java/collections/ArrayList.java
+++ src/com/java/collections/ArrayList.java
@@ -1,5 +1,15 @@
 package com.java.collections;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayList {
- 
+	ArrayList(){
+		List<String> list=Arrays.asList("Test","hari");
+		System.out.println(list);
+		}
+		
+		public static void main(String args[]) {
+			ArrayList test=new ArrayList();
+		}
 }
